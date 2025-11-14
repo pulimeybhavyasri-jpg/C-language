db1f5526fdac1532aa1e676b2b8140f5190c4f83
PULIMEY BHAVYA SRI <pulimeybhavyasri@gmail.com>
2025-11-14 11:09:46 +0530

    Add files via upload

---
+++ selecting and sorting.cpp
@@ -0,0 +1,36 @@
+#include <stdio.h>
+
+int main() {
+    int i, j, min, n, a[10], temp;
+
+    printf("Enter n value: ");
+    scanf("%d", &n);
+
+    printf("Enter the array elements: ");
+    for(i = 0; i < n; i++) {
+        scanf("%d", &a[i]);
+    }
+
+    for(i = 0; i < n-1; i++) {
+        min = i;
+        for(j = i+1; j < n; j++) {
+            if(a[j] < a[min]) {
+                min = j;
+            }
+        }
+        if(min != i) {
+            temp = a[i];
+            a[i] = a[min];
+            a[min] = temp;
+        }
+    }
+
+    printf("Sorted array: ");
+    for(i = 0; i < n; i++) {
+        printf("%d ", a[i]);
+    }
+    printf("\n");
+
+    return 0;
+}
+
