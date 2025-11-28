2f3e4eec8b04a08d241b5a738c25c6345c88ae87
PULIMEY BHAVYA SRI <pulimeybhavyasri@gmail.com>
2025-11-28 10:57:01 +0530

    Add files via upload

---
+++ write a c program for auto keyword -1.cpp
@@ -0,0 +1,11 @@
+#include<stdio.h>
+void display()
+{
+    int a=10;  // auto is implicit
+    printf("%d",a);
+}
+int main()
+{
+    display();
+    return 0;
+}
---
+++ write a c program for keyword.cpp
@@ -0,0 +1,15 @@
+//write a c program for keyword
+#include<stdio.h>
+int display()
+{
+	static int a=10;
+	a++;
+	return a;
+}
+int main()
+{
+	printf("value of a: %d\n" , display());
+	printf("value of a: %d\n" , display());
+	printf("value of a: %d\n" , display());
+	return 0;
+}
